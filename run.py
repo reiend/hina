0408930a302d4596a78fdff08a76ff68b7fac5a2
JakeLonceras <jake@matchmo.ph>
2023-08-22 00:21:45 +0800

    build with ninja and added target flags for clang

--- run.py
+++ run.py
@@ -4,32 +4,34 @@ import subprocess
 # Specify the paths
 source_dir = "."
 build_dir = "build"
-build_system = "MinGW Makefiles"
+# build_system = "MinGW Makefiles"
+build_system = "Ninja"
 
 # Create the build directory if it doesn't exist
 os.makedirs(build_dir, exist_ok=True)
 
 # Run CMake to generate the build files
 def generate_build_files():
-    cmake_command = ["cmake", "-S", source_dir, "-B", build_dir, "-G", build_system]
-    subprocess.run(cmake_command)
+    cmake_command = ["cmake", "-Wno-dev", "-S", source_dir, "-B", build_dir, "-G", build_system, "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"]
+    subprocess.run(cmake_command, shell=True)
 
 # Build the C++ program using make
 def build_with_make():
-    make_command = ["make"]
-    subprocess.run(make_command, cwd=build_dir)
+    make_command = ["ninja"]
+    # make_command = ["make"]
+    subprocess.run(make_command, cwd=build_dir, shell=True)
 
 # Run the compiled C++ program (assuming main.exe)
 def run_program(executable_name):
     cpp_program_path = os.path.join(build_dir, executable_name)
-    subprocess.run([cpp_program_path])
+    subprocess.run([cpp_program_path], shell=True)
 
 # Clean up (optional)
 def make_clean_ups():
-    subprocess.run(["make", "clean"], cwd=build_dir)
+    subprocess.run(["make", "clean"], cwd=build_dir, shell=True)
 
 generate_build_files()
 build_with_make()
 run_program("main.exe")
-make_clean_ups()
+# make_clean_ups()
 
