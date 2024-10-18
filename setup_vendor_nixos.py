218215926fb2145099614222cd6544fe673889f8
reiend <loncerasjakerei@gmail.com>
2024-10-18 15:21:28 +0800

    refactored check system nix vendor setup

--- setup_vendor_nixos.py
+++ setup_vendor_nixos.py
@@ -3,6 +3,12 @@ import os
 import shutil
 import subprocess
 
+def check_system_nixos():
+  if platform.freedesktop_os_release()["NAME"] == "NixOS":
+      return
+
+  raise OSError("setup vendor failed: make sure the system nixos")
+
 def download_vendor_nixos():
   try:
     os.environ["vendor_glfw"]
@@ -15,26 +21,27 @@ def download_vendor_nixos():
 
 def localize_vendor_nixos():
   try:
-    if platform.freedesktop_os_release()["NAME"] == "NixOS":
-      if not os.path.isdir("./vendor/glfw/include"):
-        shutil.copytree('%s/include' % os.environ["vendor_glfw"], './vendor/glfw/include', dirs_exist_ok=True)
+    if not os.path.isdir("./vendor/glfw/include"):
+      shutil.copytree('%s/include' % os.environ["vendor_glfw"], './vendor/glfw/include', dirs_exist_ok=True)
 
-      if not os.path.isdir("./vendor/glfw/lib"):
-        shutil.copytree('%s/lib' % os.environ["vendor_glfw"], './vendor/glfw/lib', dirs_exist_ok=True)
+    if not os.path.isdir("./vendor/glfw/lib"):
+      shutil.copytree('%s/lib' % os.environ["vendor_glfw"], './vendor/glfw/lib', dirs_exist_ok=True)
 
-      if not os.path.isdir("./vendor/vulkan/include"):
-        shutil.copytree('%s/include' % os.environ["vendor_vulkan_header"], './vendor/vulkan/include', dirs_exist_ok=True)
+    if not os.path.isdir("./vendor/vulkan/include"):
+      shutil.copytree('%s/include' % os.environ["vendor_vulkan_header"], './vendor/vulkan/include', dirs_exist_ok=True)
 
-      if not os.path.isdir("./vendor/vulkan/lib"):
-        shutil.copytree('%s/lib' % os.environ["vendor_vulkan_loader"], './vendor/vulkan/lib', dirs_exist_ok=True)
+    if not os.path.isdir("./vendor/vulkan/lib"):
+      shutil.copytree('%s/lib' % os.environ["vendor_vulkan_loader"], './vendor/vulkan/lib', dirs_exist_ok=True)
 
-      if not os.path.isdir("./vendor/glm/include"):
-        shutil.copytree('%s/include' % os.environ["vendor_glm"], './vendor/glm/include', dirs_exist_ok=True)
+    if not os.path.isdir("./vendor/glm/include"):
+      shutil.copytree('%s/include' % os.environ["vendor_glm"], './vendor/glm/include', dirs_exist_ok=True)
 
-      if not os.path.isdir("./vendor/gl/include"):
-        shutil.copytree('%s/include' % os.environ["vendor_gl"], './vendor/gl/include', dirs_exist_ok=True)
+    if not os.path.isdir("./vendor/gl/include"):
+      shutil.copytree('%s/include' % os.environ["vendor_gl"], './vendor/gl/include', dirs_exist_ok=True)
+
   except:
     raise RuntimeError("vendor localization failed: shell variables doesn't exist make sure to download vendors using nixpkgs and set the shell variables via nix-shell")
 
+check_system_nixos()
 download_vendor_nixos()
 localize_vendor_nixos()
