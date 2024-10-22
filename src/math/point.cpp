6fdb5fc1a4e3c3237cd29993e9a47a0666101aee
reiend <loncerasjakerei@gmail.com>
2024-10-23 05:07:59 +0800

    added assertion for creating point in 3D

--- src/math/point.cpp
+++ src/math/point.cpp
@@ -2,6 +2,8 @@
 
 template <typename T, int D>
 Point<T, D>::Point(T x, T y, T z) {
+  static_assert(D == 3, "Point<T, D> initialization failed: D must be 3");
+
   this->point[0] = x;
   this->point[1] = y;
   this->point[2] = z;
